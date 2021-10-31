ead94f37bd63a935a12216978c9c248d389ec484
namoo <namoo.developer@gmail.com>
2021-10-31 16:49:16 +0900

    Add prototype for DetailPage

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-import { Container } from "./style";
+import { Container, Header, Title, BackButton, ContentContainer } from "./style";
+import BigCard from "../../components/BigCard";
 
 function DetailPage(props) {
     return (
         <Container>
-            DetailPage
+            <Header>
+                <BackButton>&lt;</BackButton>
+                <Title>수면</Title>
+            </Header>
+            <ContentContainer>
+                <BigCard title="일별 기록">
+                    Content
+                </BigCard>
+                <BigCard title="이상치">
+                    Content
+                </BigCard>
+            </ContentContainer>
         </Container>
     );
 }
--- src/pages/DetailPage/style.js
+++ src/pages/DetailPage/style.js
@@ -4,3 +4,38 @@ export const Container = styled.div`
     display: flex;
     flex-flow: column nowrap;
 `
+
+export const Header = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    padding: 40px 30px 0px 30px;
+    justify-content: flex-start;
+    align-items: center;
+`
+export const Title = styled.h1`
+    margin: 0;
+    margin-left: 10px;
+    font-weight: 500;
+    font-size: 1.8rem;
+    color: black;
+`
+export const BackButton = styled.button`
+    width: 2rem;
+    height: 2rem;
+    line-height: 2rem;
+    background: none;
+    border: none;
+    font-size: 2rem;
+    text-align: center;
+    color: black;
+    transform: scaleX(0.5);
+`
+
+export const ContentContainer = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    padding: 30px;
+    box-sizing: border-box;
+    gap: 30px;
+`
