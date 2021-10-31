48ab86201cc4e22a479cd04228461ded6c9d9849
namoo <namoo.developer@gmail.com>
2021-10-31 16:33:59 +0900

    Add prototype for AnalysisPage

--- src/pages/AnalysisPage/index.js
+++ src/pages/AnalysisPage/index.js
@@ -1,11 +1,38 @@
 import React from "react";
 
-import { Container } from "./style";
+import { Container, ContentContainer } from "./style";
+import Header from "../../components/Header";
+import BigCard from "../../components/BigCard";
+import Tab from "../../components/Tab";
 
 function AnalysisPage(props) {
     return (
         <Container>
-            AnalysisPage
+            <Header user="456"/>
+            <ContentContainer>
+                <BigCard title="수면">
+                    Content
+                </BigCard>
+                <BigCard title="식사">
+                    Content
+                </BigCard>
+                <BigCard title="배변">
+                    Content
+                </BigCard>
+                <BigCard title="운동">
+                    Content
+                </BigCard>
+                <BigCard title="약 복용">
+                    Content
+                </BigCard>
+                <BigCard title="활동">
+                    Content
+                </BigCard>
+                <BigCard title="순이와의 대화">
+                    Content
+                </BigCard>
+            </ContentContainer>
+            <Tab selected={1}/>
         </Container>
     );
 }
