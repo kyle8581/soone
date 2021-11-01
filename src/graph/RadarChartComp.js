ac28db90de1c84200a6f84bf015c7ae24635be8f
namoo <namoo.developer@gmail.com>
2021-11-02 01:30:12 +0900

    Add home radar chart

---
+++ src/graph/RadarChartComp.js
@@ -0,0 +1,18 @@
+import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+
+function RadarChartComp(props) {
+    return (
+        <ResponsiveContainer width="90%" aspect={1}>
+            <RadarChart outerRadius="80%" data={props.data}>
+                <PolarGrid />
+                <PolarAngleAxis dataKey="habit" />
+                <PolarRadiusAxis angle={30} domain={[0, 100]} />
+                {/* <Radar name="전체" dataKey="all" fill="#000000" fillOpacity={0.2} /> */}
+                <Radar name="나" dataKey="me" fill="#4c9be0" fillOpacity={0.7}/>
+                {/* <Legend /> */}
+            </RadarChart>
+        </ResponsiveContainer>
+    );
+}
+
+export default RadarChartComp;
--- src/pages/SummaryPage/index.js
+++ src/pages/SummaryPage/index.js
@@ -7,21 +7,34 @@ import SmallCard from "../../components/SmallCard";
 import BigCard from "../../components/BigCard";
 import Tab from "../../components/Tab";
 
+import RadarChartComp from "../../graph/RadarChartComp";
+
 function SummaryPage(props) {
     const { id } = useParams();
 
     const goDetail = (type) => props.history.push(`/${id}/detail/${type}`); 
 
+    // Score analysis
+    const scoreData = [
+        { habit: '수면', me: 70, all: 88 },
+        { habit: '식사', me: 88, all: 77 },
+        { habit: '배변', me: 60, all: 63 },
+        { habit: '운동', me: 95, all: 60 },
+        { habit: '약 복용', me: 38, all: 51 },
+        { habit: '활동', me: 70, all: 52 },
+    ];
+
     return (
         <Container>
             <Header user={id}/>
             <ContentContainer>
-                <SmallCard title="수면" score={0} message="" onClick={() => goDetail('1')}/>
-                <SmallCard title="식사" score={0} message="" onClick={() => goDetail('2')}/>
-                <SmallCard title="배변" score={0} message="" onClick={() => goDetail('3')}/>
-                <SmallCard title="운동" score={0} message="" onClick={() => goDetail('4')}/>
-                <SmallCard title="약 복용" score={0} message="" onClick={() => goDetail('5')}/>
-                <SmallCard title="활동" score={0} message="" onClick={() => goDetail('6')}/>
+                <RadarChartComp data={scoreData}/>
+                <SmallCard title="수면" score={70} message="" onClick={() => goDetail('1')}/>
+                <SmallCard title="식사" score={88} message="" onClick={() => goDetail('2')}/>
+                <SmallCard title="배변" score={60} message="" onClick={() => goDetail('3')}/>
+                <SmallCard title="운동" score={95} message="" onClick={() => goDetail('4')}/>
+                <SmallCard title="약 복용" score={38} message="" onClick={() => goDetail('5')}/>
+                <SmallCard title="활동" score={70} message="" onClick={() => goDetail('6')}/>
                 <BigCard title="타이틀">
                     Content
                 </BigCard>
