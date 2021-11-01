d5f56381181c3503d7042dad26c298838048028f
namoo <namoo.developer@gmail.com>
2021-11-02 01:49:52 +0900

    Add rank card

--- src/pages/RankPage/index.js
+++ src/pages/RankPage/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import { Container, ContentContainer } from "./style";
+import { Container, ContentContainer, Row, Rank, Detail } from "./style";
 import Header from "../../components/Header";
 import BigCard from "../../components/BigCard";
 import Tab from "../../components/Tab";
@@ -9,12 +9,22 @@ import Tab from "../../components/Tab";
 function RankPage(props) {
     const { id } = useParams();
 
+    // Rank data
+    const rank = require('../../default/extra/rank.json');
+
     return (
         <Container>
             <Header user={id}/>
             <ContentContainer>
-                <BigCard title="한 달 동안의 순위">
-                    Content
+                <BigCard title="전체 순위">
+                    {
+                        rank.map((user, index) => (
+                            <Row key={index}>
+                                <Rank checked={id * 1 === user}>{index + 1}위</Rank>
+                                <Detail checked={id * 1 === user}>{user}번</Detail>
+                            </Row>
+                        ))
+                    }
                 </BigCard>
             </ContentContainer>
             <Tab id={id} selected={2}/>
--- src/pages/RankPage/style.js
+++ src/pages/RankPage/style.js
@@ -11,3 +11,24 @@ export const ContentContainer = styled.div`
     box-sizing: border-box;
     gap: 30px;
 `
+
+export const Row = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    margin-top: 10px;
+    padding-bottom: 10px;
+    box-sizing: border-box;
+    border-bottom: 1px solid lightgray;
+`
+export const Rank = styled.p`
+    margin: 0;
+    flex: 1 1 0;
+    font-weight: 600;
+    font-size: 1.1rem;
+    color: ${props => props.checked ? "blue" : "black"};
+`
+export const Detail = styled.p`
+    margin: 0;
+    font-size: 1.1rem;
+    color: ${props => props.checked ? "blue" : "black"};
+`
