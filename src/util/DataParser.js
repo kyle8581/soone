62245bb47dbe501a032c2be9363aee2082edc0e0
namoo <namoo.developer@gmail.com>
2021-11-05 10:01:36 +0900

    Apply v2 summary data

--- src/util/DataParser.js
+++ src/util/DataParser.js
@@ -10,34 +10,34 @@ export function getSleepData(id) {
     return { daily, summary, outlier, all };
 }
 export function getMealData(id) {
-    const daily = require(`../tempData/meal/daily.json`); // Temp
-    const summary = require(`../tempData/meal/monthly.json`); // Temp
-    const outlier = require(`../tempData/meal/outlier.json`); // Temp
-    const all = require(`../tempData/meal/all_user_average.json`); // Temp
+    const daily = require(`../data/${id}/meal/daily.json`);
+    const summary = require(`../data/${id}/meal/monthly.json`);
+    const outlier = require(`../data/${id}/meal/outlier.json`);
+    const all = require(`../data/all_user/meal/all_user_average.json`);
 
     return { daily, summary, outlier, all };
 }
 export function getToiletData(id) {
-    const daily = require(`../tempData/toilet/daily.json`); // Temp
-    const summary = require(`../tempData/toilet/monthly.json`); // Temp
-    const outlier = require(`../tempData/toilet/outlier.json`); // Temp
-    const all = require(`../tempData/toilet/all_user_average.json`); // Temp
+    const daily = require(`../data/${id}/toilet/daily.json`);
+    const summary = require(`../data/${id}/toilet/monthly.json`);
+    const outlier = require(`../data/${id}/toilet/outlier.json`);
+    const all = require(`../data/all_user/toilet/all_user.json`);
 
     return { daily, summary, outlier, all };
 }
 export function getExerciseData(id) {
-    const daily = require(`../tempData/exercise/daily.json`); // Temp
-    const summary = require(`../tempData/exercise/monthly.json`); // Temp
-    const outlier = require(`../tempData/exercise/outlier.json`); // Temp
-    const all = require(`../tempData/exercise/all_user_average.json`); // Temp
+    const daily = require(`../data/${id}/workout/daily.json`);
+    const summary = require(`../data/${id}/workout/monthly.json`);
+    const outlier = require(`../data/${id}/workout/outlier.json`);
+    const all = require(`../data/all_user/workout/all_user.json`);
 
     return { daily, summary, outlier, all };
 }
 export function getMedicineData(id) {
-    const daily = require(`../tempData/medicine/daily.json`); // Temp
-    const summary = require(`../tempData/medicine/monthly.json`); // Temp
-    const outlier = require(`../tempData/medicine/outlier.json`); // Temp
-    const all = require(`../tempData/medicine/all_user_average.json`); // Temp
+    const daily = require(`../data/${id}/pill/daily.json`);
+    const summary = require(`../data/${id}/pill/monthly.json`);
+    const outlier = require(`../data/${id}/pill/outlier.json`);
+    const all = require(`../data/all_user/pill/all_user.json`);
 
     return { daily, summary, outlier, all };
 }
@@ -60,14 +60,14 @@ export function getChatData(id) {
 
 // 타이틀
 export function getTitleData(id) {
-    const data = require(`../tempData/extra/title.json`); // Temp
+    const data = require(`../data/${id}/title/title.json`);
 
     return convertArray(data);
 }
 
 // 순위
 export function getRankData(id) {
-    const data = require(`../tempData/extra/rank.json`); // Temp
+    const data = require(`../data/all_user/ranking/ranking.json`);
 
     return data;
 }
